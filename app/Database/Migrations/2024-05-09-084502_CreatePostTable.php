3304585c481fbe2031bcbce84875c9d3957db0cc
Julius <juliushubilla@gmail.com>
2024-05-09 17:29:42 +0800

    first commit

---
+++ app/Database/Migrations/2024-05-09-084502_CreatePostTable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'=>[
+                'type'=>'INT',
+                'constraint'=>11,
+                'unsigned'=>true,
+                'auto_increment'=>true
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
---
+++ app/Models/PostModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostModel extends Model
+{
+    protected $table            = 'posts';
+    protected $primaryKey       = 'id';
+    protected $allowedFields    = [];
+
+
+}
